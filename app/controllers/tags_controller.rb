472107ef8d0acc54fae719746f76cdb35a54f729
Justin Mullis <justin.mullis@gmail.com>
2015-09-27 18:43:05 -0400

    authentication

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,5 +1,7 @@
 class TagsController < ApplicationController
 
+  before_filter :require_login, only: [:destroy]
+
   def index
     @tags = Tag.all
   end
@@ -7,6 +9,19 @@ class TagsController < ApplicationController
 
   def show
     @tag = Tag.find(params[:id])
+  end
+
+
+  def destroy
+    tag = Tag.find(params[:id])
+    tag.destroy
+
+    Tagging.where(tag_id: tag.id).find_each do |tagging|
+      tagging.destroy
+    end
+
+    flash.notice = "Removed #{tag.name}."
+    redirect_to tags_path
   end
 
 end
